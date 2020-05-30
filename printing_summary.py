b1986d1c362dc7610b2eb74890c4c6f9acf7921c
Trung Duc <trungduc@Trungs-MacBook-Pro.local>
2020-05-30 20:38:18 +0700

    read each line to a list

--- printing_summary.py
+++ printing_summary.py
@@ -1,6 +1,18 @@
 import sys
+import re
 
-def verify_file_input():
+
+def read_each_line_to_list(file):
+    result = []
+    for line in file:
+        arr_str = line.strip().split(',')
+        result.append(arr_str)
+    file.close()
+    return result
+
+
+
+def main():
     if len(sys.argv) < 3:
         print('Require a file name')
         exit(1)
@@ -15,5 +27,12 @@ def verify_file_input():
         if readable == False:
             print('Cannot read the file')
             exit(1)
+    
+    data = read_each_line_to_list(f)
+    print(data)
 
-verify_file_input()
+
+if __name__ == '__main__':
+    main()
+
+
